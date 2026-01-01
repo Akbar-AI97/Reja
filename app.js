a1532e63fff54fcd5718fc7c46c2a01fa620fd86
Akbar-AI97 <akbaratakhanov@gmail.com>
2026-01-01 12:57:25 +0900

    feat: connecting to mongoDB atlas database

---
+++ app.js
@@ -0,0 +1,52 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+
+// MongoDB call
+const db = require("./server").db(); //Qalam
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
+
+//1: Entry code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+//2: Session code
+
+//3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs"); //BSSR: Backend Server Side Rendering (Creating Frontend in the Backend side)
+
+//4: Routing code
+// app.get("/hello", function(req, res) {
+//     res.end("<h1>Hello world</h1>");
+// });
+// app.get("/gift", function(req, res) {
+//     res.end("<h1>You're in the gifts section</h1>");
+// });
+
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test: "success"});
+})
+
+app.get("/author", (req, res) => {
+    res.render("author", {user: user});
+})
+
+app.get("/", function(req, res) {
+    res.render("reja");
+})
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,53 +1,50 @@
-console.log("Web serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+//MongoDB define
+const {MongoClient} = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
+
+const connectionString =  "mongodb+srv://tekken:bKLtDjTOpLYNX6PW@cluster0.l2tiwhc.mongodb.net/Reja?retryWrites=true&w=majority";
+
+const client = new MongoClient(connectionString);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("mongoDB connection succeed!");
+        // console.log(client);
+        module.exports = client;
+
+        const app = require("./app");
+
+        const server = http.createServer(app);
+        const PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(`The server is running on port: ${PORT}, http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.log("ERROR connecting to mongoDB database: ", err);
     }
-});
+}
 
-//1: Entry code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+run();
 
+//------- mongodb v->3.2.6 dagi logic ishlamadi (couldn't resolve DNS SRV problems)
+// mongodb.connect(connectionString, 
+//     { useNewUrlParser: true, useUnifiedTopology: true },
+//     (err, client) => {
+//         if(err) {
+//             console.log("ERROR connecting to mongoDB database: ", err);
+//         } else {
+//             console.log("mongoDB connection succeed!");
 
-//2: Session code
+//             const app = require("./app");
 
-//3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs"); //BSSR: Backend Server Side Rendering (Creating Frontend in the Backend side)
+//             const server = http.createServer(app);
+//             const PORT = 3000;
+//             server.listen(PORT, function() {
+//                 console.log(`The server is running on port: ${PORT}, http://localhost:${PORT}`);
+//             });
+//         }
+//     }
+// );
 
-//4: Routing code
-// app.get("/hello", function(req, res) {
-//     res.end("<h1>Hello world</h1>");
-// });
-// app.get("/gift", function(req, res) {
-//     res.end("<h1>You're in the gifts section</h1>");
-// });
-
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
-})
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-})
-
-app.get("/", function(req, res) {
-    res.render("reja");
-})
-
-const server = http.createServer(app);
-const PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`The server is running on port: ${PORT}, http://localhost:${PORT}`);
-});
